6a4b37a2d84f2fcb39c7182b36e5cf31b3bca2ef
Shreyas M S <shreyasmadappady20@gmail.com>
2025-03-26 22:37:26 +0530

    c

--- app/api/upload/Upload.ts
+++ app/api/upload/Upload.ts
@@ -14,7 +14,7 @@ export default async function UPLOAD({
   //   "AIzaSyA2bW3jhFQMlSRZvRyXZCTLbYczeoJruzc",
   //   "AIzaSyBwzqeVWzLPb-TjfbaqV5UIEBbN-xuF7Lg",
   // ];
-  const apiKeys = ["AIzaSyBnrCGypjsFCx05Qvsc5n73gAM6eTEvaaQ"];
+  const apiKeys = ["AIzaSyCTAYK_SvenZa8kVoNvk3LepQl3efGfqTc"];
 
   // Generate a random index
   const randomIndex = Math.floor(Math.random() * apiKeys.length);
